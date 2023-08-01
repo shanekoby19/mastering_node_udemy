27593272b384985b4d9f1d9f15869e1f8f268362
Shane Kobylecky <skobylecky1@gmail.com>
2023-07-31 23:36:22 -0700

    Add npm packages to our project

--- Section 3 - Understanding the Basics/server.js
+++ Section 3 - Understanding the Basics/server.js
@@ -4,4 +4,10 @@ const router = require('./router');
 const server = http.createServer(router);
 http.createServer()
 
+
+process.on('SIGINT', () => {
+    console.log('Closing the server gracefully after SIGINT (Ctrl + C)')
+    server.close();
+});
+
 server.listen(3000);
